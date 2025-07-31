49013f84330a635451afafb9f2054fec741a4bd6
Amit <amituliel16@gmail.com>
2025-07-31 17:22:14 +0300

    Add function to create <td> elements and simplify loop

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ function addBookToLibrary(title, author, pages, isRead) {
     books.push(book);
 }
 
+function createTd(text) {
+    const td = document.createElement('td');
+    td.textContent = text;
+    return td;
+}
+
 const book1 = addBookToLibrary("The Hunger Games", "Suzanne Collins", 374, true);
 const book2 = addBookToLibrary("The Witcher", "Andrzej Sapkowski", 295, false);
 const book3 = addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, true);
@@ -30,26 +36,15 @@ const tbody = document.querySelector('tbody');
 for (const book of books) {
     const tr = document.createElement('tr');
 
-    const id = document.createElement('td');
-    id.textContent = book.id;
+    const values = [
+        book.id,
+        book.title,
+        book.author,
+        book.pages,
+        book.isRead ? '✅' : '❌',
+    ];
 
-    const title = document.createElement('td');
-    title.textContent = book.title;
-
-    const author = document.createElement('td');
-    author.textContent = book.author;
-
-    const pages = document.createElement('td');
-    pages.textContent = book.pages;
-
-    const isRead = document.createElement('td');
-    isRead.textContent = book.isRead ? '✅' : '❌';
-
-    tr.appendChild(id);
-    tr.appendChild(title);
-    tr.appendChild(author);
-    tr.appendChild(pages);
-    tr.appendChild(isRead);
-
+    values.forEach(value => tr.appendChild(createTd(value)));
+    
     tbody.appendChild(tr);
 }
